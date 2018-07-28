3a81355cd82b3ad1667de9f9bd9b4139943c9453
Thibaud Colas <thibaudcolas@gmail.com>
2018-07-28 10:04:50 +0300

    chore(release): first commit

---
+++ dangerfile.js
@@ -0,0 +1,22 @@
+import path from "path";
+import { danger, message, warn, fail, schedule } from "danger";
+
+const hasLabels = danger.github.issue.labels.length !== 0;
+
+if (!hasLabels) {
+  message("What labels should we add to this PR?");
+}
+
+// Fails if the description is too short.
+if (!danger.github.pr.body || danger.github.pr.body.length < 10) {
+  fail(":grey_question: This pull request needs a description.");
+}
+
+// Warns if the PR title contains [WIP]
+const isWIP = danger.github.pr.title.includes("WIP");
+if (isWIP) {
+  const title = ":construction_worker: Work In Progress";
+  const idea =
+    "This PR appears to be a work in progress, and may not be ready to be merged yet.";
+  warn(`${title} - <i>${idea}</i>`);
+}
---
+++ src/index.js
@@ -0,0 +1,3 @@
+const greet = (name = 'World') => `Hello, ${name}!`;
+
+export { greet };
